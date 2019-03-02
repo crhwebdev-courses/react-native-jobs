24636b303ea13b81c4507211445e4c681f2abaa2
Carl Hain <crhain@gmail.com>
2019-03-02 09:41:50 -0600

    Add nested navigation to main flow inside of MainNavigation component

--- MainNavigation.js
+++ MainNavigation.js
@@ -2,11 +2,22 @@ import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 import WelecomScreen from './screens/WelecomeScreen';
 import AuthScreen from './screens/AuthScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
 
-//create TabNavigator
-const BottomNavigator = createBottomTabNavigator({
-  welecome: WelecomScreen,
-  auth: AuthScreen
+//Tab navigation for main app flow
+const MainFlowNavigator = createBottomTabNavigator({
+  map: MapScreen,
+  deck: DeckScreen
 });
 
-export default createAppContainer(BottomNavigator);
+//Top level Tab navigation which contains initial secreens (Welcome and Auth)
+// and main flow
+const MainNavigator = createBottomTabNavigator({
+  welecome: WelecomScreen,
+  auth: AuthScreen,
+  main: MainFlowNavigator
+});
+
+//Note: we only need to wrap top level navigation in createAppContainer
+export default createAppContainer(MainNavigator);

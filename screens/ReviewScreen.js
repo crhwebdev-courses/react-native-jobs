14ee07a76123f418f690132d80842fa9d8c5d504
Carl Hain <crhain@gmail.com>
2019-03-02 10:48:30 -0600

    Add navigation button to ReviewScreen that navigates to SettingsScreen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
-  static navigationOptions = {
-    title: 'Review Jobs',
-    headerRight: <Text>Right Button</Text>
+  /*
+    navigationOptions can take either an configuration object or an arrow function
+    that returns a configuration object.
+
+    use an arrow function if you need to make items on the components props object 
+    available within the configuration object -- for instance, passing the navigation object
+    to handle button presses
+   */
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Review Jobs',
+      headerRight: (
+        <Button
+          title="Settings"
+          onPress={() => navigation.navigate('settings')}
+        />
+      )
+    };
   };
 
   render() {

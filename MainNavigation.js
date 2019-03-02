8fb782e509d76e439090a55f7a2e6027d25fca50
Carl Hain <crhain@gmail.com>
2019-03-02 10:20:57 -0600

    Add StackNavigator for ReviewScreen and SettingsScreen to MainNavigation component

--- MainNavigation.js
+++ MainNavigation.js
@@ -1,14 +1,27 @@
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+  createAppContainer
+} from 'react-navigation';
 
 import WelecomScreen from './screens/WelecomeScreen';
 import AuthScreen from './screens/AuthScreen';
 import MapScreen from './screens/MapScreen';
 import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+//Stack navigation for ReviewScreen and SettingsScreen
+const ReviewFlowNavigator = createStackNavigator({
+  review: ReviewScreen,
+  settings: SettingsScreen
+});
 
 //Tab navigation for main app flow
 const MainFlowNavigator = createBottomTabNavigator({
   map: MapScreen,
-  deck: DeckScreen
+  deck: DeckScreen,
+  review: ReviewFlowNavigator
 });
 
 //Top level Tab navigation which contains initial secreens (Welcome and Auth)

4e168e64ab8a8791c1b0da77b58c3f812445a025
Carl Hain <crhain@gmail.com>
2019-03-03 10:46:00 -0600

    Add Styling to button on Slides component

--- components/Slides.js
+++ components/Slides.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { View, Text, ScrollView, Dimensions, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1) {
+      return (
+        <Button
+          title="Let's Get Huntin'!"
+          buttonStyle={styles.buttonStyle}
+          containerStyle={styles.buttonContainerStyle}
+          titleStyle={styles.buttonTitleStyle}
+          raised
+        />
+      );
+    }
+  }
+
   renderSlides() {
-    return this.props.data.map(slide => {
+    return this.props.data.map((slide, index) => {
       return (
         <View
           key={slide.text}
           style={[styles.slideStyle, { backgroundColor: slide.color }]}
         >
           <Text style={styles.textStyle}>{slide.text}</Text>
+          {this.renderLastSlide(index)}
         </View>
       );
     });
@@ -26,7 +42,7 @@ class Slides extends Component {
   }
 }
 
-const styles = {
+const styles = StyleSheet.create({
   slideStyle: {
     flex: 1,
     justifyContent: 'center',
@@ -36,7 +52,17 @@ const styles = {
   textStyle: {
     fontSize: 30,
     color: 'white'
+  },
+  buttonStyle: {
+    backgroundColor: 'orange'
+  },
+  buttonContainerStyle: {
+    marginTop: 30
+  },
+  buttonTitleStyle: {
+    fontSize: 20,
+    padding: 5
   }
-};
+});
 
 export default Slides;

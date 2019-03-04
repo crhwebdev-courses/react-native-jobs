96af040405977715405a9148534e4fdda863522f
Carl Hain <crhain@gmail.com>
2019-03-04 11:15:14 -0600

    Hook up facebookLogin action to AuthScreen so that it fires when component loads

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
+import { facebookLogin } from '../actions';
+
 import styles from './styles';
 
 class AuthScreen extends Component {
+  componentWillMount() {
+    this.props.facebookLogin();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>AuthScreen</Text>
         <Text>AuthScreen</Text>
         <Text>AuthScreen</Text>
         <Text>AuthScreen</Text>
@@ -16,4 +22,7 @@ class AuthScreen extends Component {
   }
 }
 
-export default AuthScreen;
+export default connect(
+  null,
+  { facebookLogin }
+)(AuthScreen);

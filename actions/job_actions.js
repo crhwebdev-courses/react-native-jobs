8c750ab29142e70af2398fa133e553592ecb7fc2
Carl Hain <crhain@gmail.com>
2019-03-06 09:33:41 -0600

    Add navigation to deck screen after successfully retrieving jobs for a region

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -20,7 +20,7 @@ const buildJobsUrl = zip => {
   return `${JOB_ROOT_URL}${query}`;
 };
 
-export const fetchJobs = region => async dispatch => {
+export const fetchJobs = (region, callback) => async dispatch => {
   try {
     //NOTE: use expo location api instead
     let zip = await reverseGeocode(region);
@@ -29,6 +29,7 @@ export const fetchJobs = region => async dispatch => {
 
     let { data } = await axios.get(url);
     dispatch({ type: FETCH_JOBS, payload: data });
+    callback();
   } catch (error) {
     console.log(error);
   }

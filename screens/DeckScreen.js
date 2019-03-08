7588f7f65e07d263a50864e7f752a8e8f8e5791d
Carl Hain <crhain@gmail.com>
2019-03-07 22:24:36 -0600

    Add Swipe component to DeckScreen and add a data property that points to the list of jobs--currently broken as Swipe needs additional props

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
+import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
   render() {
     return (
       <View>
-        <Text>Decks</Text>
+        <Swipe data={this.props.jobs} />
       </View>
     );
   }

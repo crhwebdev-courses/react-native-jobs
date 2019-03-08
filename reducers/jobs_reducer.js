14043323a4cbb9ee3b6bac645ac236bd34a6d5e8
Carl Hain <crhain@gmail.com>
2019-03-07 22:01:06 -0600

    Connect DeckScreen component to jobs state in redux

--- reducers/jobs_reducer.js
+++ reducers/jobs_reducer.js
@@ -7,7 +7,7 @@ INITIAL_STATE = {
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case FETCH_JOBS:
-      return action.payload;
+      return { results: action.payload };
     default:
       return state;
   }
--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
 
 class DeckScreen extends Component {
   render() {
     return (
       <View>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+        <Text>Decks</Text>
       </View>
     );
   }
 }
 
-export default DeckScreen;
+const mapStateToProps = ({ jobs }) => {
+  return { jobs: jobs.results };
+};
+
+export default connect(mapStateToProps)(DeckScreen);

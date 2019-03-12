aa7e43e539225ab0a080e793598938da2a3084ae
Carl Hain <crhain@gmail.com>
2019-03-12 13:15:45 -0500

    Connect SettingsScreen to clearLikedJobs action and place a button on screen that fires action when pressed

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { clearLikedJobs } from '../actions';
 
 class SettingsScreen extends Component {
   render() {
     return (
       <View>
-        <Text>SettingsScreen</Text>
-        <Text>SettingsScreen</Text>
-        <Text>SettingsScreen</Text>
-        <Text>SettingsScreen</Text>
-        <Text>SettingsScreen</Text>
+        <Button
+          title="Clear Jobs"
+          onPress={() => this.props.clearLikedJobs()}
+        />
       </View>
     );
   }
 }
 
-export default SettingsScreen;
+export default connect(
+  null,
+  { clearLikedJobs }
+)(SettingsScreen);

66f92151eb90d76160e7fe6bf96eeae27b9e34d2
Carl Hain <crhain@gmail.com>
2019-03-12 13:06:30 -0500

    Add CLEAR_LIKED_JOBS case to likes-reducer that resets likedJobs state

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,10 +1,12 @@
-import { LIKE_JOB } from '../actions/types';
+import { LIKE_JOB, CLEAR_LIKED_JOBS } from '../actions/types';
 import _ from 'lodash';
 
 export default function(state = [], action) {
   switch (action.type) {
     case LIKE_JOB:
       return _.uniqBy([action.payload, ...state], 'id');
+    case CLEAR_LIKED_JOBS:
+      return [];
     default:
       return state;
   }

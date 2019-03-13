1da117658efbf9d30f5e591f5c6b100c1fc6b9b8
Carl Hain <crhain@gmail.com>
2019-03-13 10:34:36 -0500

    Add comment about purging persisted data to store/index.js

--- store/index.js
+++ store/index.js
@@ -16,6 +16,7 @@ const store = createStore(
 //sets up store to watch for changes to specific pieces of state
 // autoRehydrate (set up in createStore call above) watches redux for changes
 // to state and issues REHYDRATE action type with a payload taken from AsyncStorage
+// NOTE: add .purge() to end of persistStore call to purge persisted data from application
 persistStore(store, { storage: AsyncStorage, whitelist: ['likedJobs'] });
 
 export default store;

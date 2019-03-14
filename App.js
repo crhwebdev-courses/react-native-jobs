8174d5a8f1bcb691a5e623f145ffa2955f92e474
Carl Hain <crhain@gmail.com>
2019-03-14 16:50:04 -0500

    Wire up services/push_notifications to App.js

--- App.js
+++ App.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import MainNavigation from './MainNavigation';
 import store from './store';
+import registerForNotifications from './services/push_notifications';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    registerForNotifications();
+  }
+
   render() {
     return (
       <Provider store={store}>

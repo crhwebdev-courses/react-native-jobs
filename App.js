d9b096a5e15fe17e110bee33b6d40a5837edc941
Carl Hain <crhain@gmail.com>
2019-03-14 16:54:20 -0500

    Add notification listener to App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Notifications } from 'expo';
+import { Alert } from 'react-native';
 import MainNavigation from './MainNavigation';
 import store from './store';
 import registerForNotifications from './services/push_notifications';
@@ -7,6 +9,16 @@ import registerForNotifications from './services/push_notifications';
 export default class App extends React.Component {
   componentDidMount() {
     registerForNotifications();
+    Notifications.addListener(notification => {
+      const {
+        data: { text },
+        origin
+      } = notification;
+
+      if (origin === 'recieved' && text) {
+        Alert.alert('New Push Notification', text, [{ text: 'Ok.' }]);
+      }
+    });
   }
 
   render() {
